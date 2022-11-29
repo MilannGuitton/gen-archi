59f55712f9d9f3a3c2df14851767cb77c62a5e22
Liann Pelhate <liann.pelhate@epita.fr>
2022-11-29 10:26:07 +0100

    [ADD][DB] Connection to mariadb for info

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi import Request
+import mariadb
+import sys
 
 app = FastAPI()
 origins = ["*"]
@@ -16,6 +18,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+try:
+    conn = mariadb.connect(
+        user="tryhard",
+        password="1234",
+        host="10.0.0.30",
+        port=3306,
+        database="mariondb"
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+cur = conn.cursor()
+
+try:
+    cur.execute("CREATE TABLE IF NOT EXISTS scores (id SERIAL PRIMARY KEY, name VARCHAR(100), score INT)")
+except mariadb.Error as e: 
+    print(f"Error: {e}")
+
 import csv
 
 class List(BaseModel):
@@ -27,16 +48,26 @@ mylist = "0"
 
 @app.get("/")
 def read_root():
-    f = open("list.csv", "r")
+    '''f = open("../list.csv", "r")
     print(f.readlines())
+    return 0'''
+    try:
+        cur.execute("SELECT * FROM test")
+        for (id, name, score) in cur:
+            print(f"ID: {id}, Name: {name}, Score: {score}")
+    except mariadb.Error as e:
+        print(f"Error: {e}")
     return 0
 
 @app.post('/')
 async def main(request: Request):
     res = await request.json()
-    with open('./list.csv', 'a', newline='') as csvfile:
+    '''with open('../list.csv', 'a', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=' ',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow([res["name"], res["score"]])
+        spamwriter.writerow([res["name"], res["score"]])'''
+    try:
+        cur.execute("INSERT INTO scores VALUES ('{}', {})".format(res["name"], res["score"]))
+    except mariadb.Error as e: 
+        print(f"Error: {e}")
     return await request.json()
-
