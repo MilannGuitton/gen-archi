ee5d6bbdcb98795fea3b69ec8512f18d41850f2e
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-01-05 15:39:09 +0100

    Fix format

--- third_platform/website.tf
+++ third_platform/website.tf
@@ -4,9 +4,9 @@
 
 
 resource "aws_s3_object" "website_files" {
-for_each      = fileset("${var.static_website_path}/", "**/*.*")
-bucket   = aws_s3_bucket.website.id
-key           = replace(each.value, "${var.static_website_path}/", "")
-source        = "${var.static_website_path}/${each.value}"
-etag          = filemd5("${var.static_website_path}/${each.value}")
+  for_each = fileset("${var.static_website_path}/", "**/*.*")
+  bucket   = aws_s3_bucket.website.id
+  key      = replace(each.value, "${var.static_website_path}/", "")
+  source   = "${var.static_website_path}/${each.value}"
+  etag     = filemd5("${var.static_website_path}/${each.value}")
 }
