84cd765746dd8b2b3032f0f1d9a426398c4b5e36
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-01-05 15:38:37 +0100

    Add file upload to s3

--- third_platform/website.tf
+++ third_platform/website.tf
@@ -1,16 +1,12 @@
 # --- Website Section --- #
 
 # ----------------------------------------------------------------- Upload --- #
-#
-locals {
-  folder_files = flatten([for d in flatten(fileset("${path.module}/${var.static_website_path}/*", "*")) : trim(d, "../")])
-}
 
-resource "aws_s3_object" "websitefiles" {
-  for_each = { for idx, file in local.folder_files : idx => file }
-  bucket   = aws_s3_bucket.website.id
 
-  key    = "/${var.static_website_path}/${each.value}"
-  source = "${path.module}/${var.static_website_path}/${each.value}"
-  etag   = "${path.module}/${var.static_website_path}/${each.value}"
+resource "aws_s3_object" "website_files" {
+for_each      = fileset("${var.static_website_path}/", "**/*.*")
+bucket   = aws_s3_bucket.website.id
+key           = replace(each.value, "${var.static_website_path}/", "")
+source        = "${var.static_website_path}/${each.value}"
+etag          = filemd5("${var.static_website_path}/${each.value}")
 }
