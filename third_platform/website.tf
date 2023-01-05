4b81e04613fb325152aa170d2b948b6d6fd4fba9
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-01-05 15:49:25 +0100

    Fix file hash limitation of 16mb

--- third_platform/website.tf
+++ third_platform/website.tf
@@ -2,11 +2,10 @@
 
 # ----------------------------------------------------------------- Upload --- #
 
-
 resource "aws_s3_object" "website_files" {
-  for_each = fileset("${var.static_website_path}/", "**/*.*")
-  bucket   = aws_s3_bucket.website.id
-  key      = replace(each.value, "${var.static_website_path}/", "")
-  source   = "${var.static_website_path}/${each.value}"
-  etag     = filemd5("${var.static_website_path}/${each.value}")
+  for_each    = fileset("${var.static_website_path}/", "**/*.*")
+  bucket      = aws_s3_bucket.website.id
+  key         = replace(each.value, "${var.static_website_path}/", "")
+  source      = "${var.static_website_path}/${each.value}"
+  source_hash = filemd5("${var.static_website_path}/${each.value}")
 }
