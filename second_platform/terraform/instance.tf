226dce4576fb98168d086bc60cb871b6fe708cd7
Milann Guitton <milann.guitton@epita.fr>
2023-01-06 17:53:08 +0100

    [ADD][TF] Restore monitor and db

--- second_platform/terraform/instance.tf
+++ second_platform/terraform/instance.tf
@@ -6,8 +6,15 @@ locals {
   image_id           = "ami-0493936afbe820b28"
   instance_type      = "t2.micro"
   front_ips          = ["10.0.0.10", "10.0.32.10", "10.0.64.10"]
+  back_ip           = "10.0.0.20"
+  db_ip             = "10.0.0.30"
+  monitor_ip        = "10.0.0.40"
   frontend_user_data = file("./frontend-cloud-init.yml")
   bastion_user_data  = file("./bastion-cloud-init.yml")
+
+  db_user_data      = file("./db-cloud-init.yml")
+  backend_user_data = file("./backend-cloud-init.yml")
+  monitor_user_data = file("./monitor-cloud-init.yml")
 
 }
 
@@ -51,6 +58,72 @@ resource "aws_instance" "frontend" {
 
   tags = {
     Name      = "Frontend-${count.index}",
+    Terraform = "true"
+  }
+}
+
+
+# ---------------------------------------------------------------- Backend --- #
+
+resource "aws_instance" "backend" {
+  ami           = local.image_id
+  instance_type = local.instance_type
+  key_name      = aws_key_pair.kp_sigl_admin.key_name
+  user_data     = local.backend_user_data
+
+  subnet_id  = module.vpc.public_subnets[0]
+  private_ip = local.back_ip
+
+  vpc_security_group_ids = [
+    aws_security_group.allow_all.id
+  ]
+
+  tags = {
+    Name      = "Backend",
+    Terraform = "true"
+  }
+}
+
+
+# --------------------------------------------------------------- Database --- #
+
+resource "aws_instance" "db" {
+  ami           = local.image_id
+  instance_type = local.instance_type
+  key_name      = aws_key_pair.kp_sigl_admin.key_name
+  user_data     = local.db_user_data
+
+  subnet_id  = module.vpc.public_subnets[0]
+  private_ip = local.db_ip
+
+  vpc_security_group_ids = [
+    aws_security_group.allow_all.id
+  ]
+
+  tags = {
+    Name      = "DB",
+    Terraform = "true"
+  }
+}
+
+
+# ---------------------------------------------------------------- Monitor --- #
+
+resource "aws_instance" "monitor" {
+  ami           = local.image_id
+  instance_type = local.instance_type
+  key_name      = aws_key_pair.kp_sigl_admin.key_name
+  user_data     = local.monitor_user_data
+
+  subnet_id  = module.vpc.public_subnets[0]
+  private_ip = local.monitor_ip
+
+  vpc_security_group_ids = [
+    aws_security_group.allow_all.id
+  ]
+
+  tags = {
+    Name      = "Monitor",
     Terraform = "true"
   }
 }
