166be3e7592d70c8ff68486a49356bf6238fc702
Milann Guitton <milann.guitton@epita.fr>
2023-01-06 16:25:40 +0100

    [UPDATE][TF] Single nat gateway in VPC

--- second_platform/terraform/vpc.tf
+++ second_platform/terraform/vpc.tf
@@ -32,8 +32,7 @@ module "vpc" {
   ]
 
   enable_nat_gateway     = true
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = false
+  single_nat_gateway     = true
 
   enable_vpn_gateway = false
 
