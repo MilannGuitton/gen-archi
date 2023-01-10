7b71fbfe183cf63d28f9ef7ad6d2374bcd551130
Milann Guitton <milann.guitton@epita.fr>
2023-01-10 20:30:49 +0100

    [ADD][TF] Front and back records

---
+++ second_platform/terraform/records.tf
@@ -0,0 +1,29 @@
+# -------------------------------------------------------- Records section --- #
+
+# ---------------------------------------------- p2-backend.aws.tryhard.fr --- #
+
+resource "aws_route53_record" "backend" {
+  allow_overwrite = true
+  name            = "p2-backend.aws.tryhard.fr"
+  ttl             = 300
+  type            = "CNAME"
+  zone_id         = data.aws_route53_zone.aws_tryhard_fr.zone_id
+
+  records = [
+    module.alb-backend.lb_dns_name,
+  ]
+}
+
+# ---------------------------------------------- p2-frontend.aws.tryhard.fr --- #
+
+resource "aws_route53_record" "frontend" {
+  allow_overwrite = true
+  name            = "p2-frontend.aws.tryhard.fr"
+  ttl             = 300
+  type            = "CNAME"
+  zone_id         = data.aws_route53_zone.aws_tryhard_fr.zone_id
+
+  records = [
+    module.alb-frontend.lb_dns_name,
+  ]
+}
