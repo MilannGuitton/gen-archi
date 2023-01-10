3c85154af02107acf7c0971dfe9ca36081ce950b
Milann Guitton <milann.guitton@epita.fr>
2023-01-10 20:30:07 +0100

    [UPDATE][TF] TLS certificate data source

--- second_platform/terraform/certs.tf
+++ second_platform/terraform/certs.tf
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------ Certs --- #
 
 data "aws_acm_certificate" "cert" {
-  domain    = "genarchi.aws.tryhard.fr"
+  domain    = "p2-backend.aws.tryhard.fr"
   statuses  = ["ISSUED"]
   key_types = ["RSA_2048"]
 }
