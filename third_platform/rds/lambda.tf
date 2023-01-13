a93f715c8f143b7ef9d0966022896ea0642ef44f
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-01-13 11:48:00 +0100

    Add new iam file

--- third_platform/rds/lambda.tf
+++ third_platform/rds/lambda.tf
@@ -9,63 +9,12 @@ locals {
   }
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "Spacelift_Test_Lambda_Function_Role"
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "lambda.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "iam_policy_for_lambda" {
- name         = "aws_iam_policy_for_terraform_aws_lambda_role"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": [
-       "logs:CreateLogGroup",
-       "logs:CreateLogStream",
-       "logs:PutLogEvents"
-     ],
-     "Resource": "arn:aws:logs:*:*:*",
-     "Effect": "Allow"
-   }
- ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
- role        = aws_iam_role.lambda_role.name
- policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
-}
-
 data "archive_file" "zip_get" {
   type        = "zip"
   source_dir  = "${path.module}/python/get"
   output_path = "${path.module}/python/get.zip"
 }
 
-data "archive_file" "zip_post" {
-  type        = "zip"
-  source_dir  = "${path.module}/python/post"
-  output_path = "${path.module}/python/post.zip"
-}
 
 resource "aws_lambda_function" "terraform_lambda_get" {
 filename                       = "${path.module}/python/get.zip"
@@ -82,6 +31,12 @@ depends_on                     = [aws_iam_role_policy_attachment.attach_iam_poli
       ENDPOINT = local.endpoint
     }
   }
+}
+
+data "archive_file" "zip_post" {
+  type        = "zip"
+  source_dir  = "${path.module}/python/post"
+  output_path = "${path.module}/python/post.zip"
 }
 
 resource "aws_lambda_function" "terraform_lambda_post" {
