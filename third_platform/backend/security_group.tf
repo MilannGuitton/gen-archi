837093962fe53ea9fde3962b07e03d4256258d12
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-01-15 18:05:33 +0100

    Add more restrictive sec group for lambda

--- third_platform/backend/security_group.tf
+++ third_platform/backend/security_group.tf
@@ -8,23 +8,13 @@ module "sg_lambda_mysql" {
   description = "MySQL security group"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      description = "Allow all inbound traffic"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
-
   egress_with_cidr_blocks = [
     {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      description = "Allow all outbound traffic"
-      cidr_blocks = "0.0.0.0/0"
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "TCP"
+      description = "MySQL egress access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
     }
   ]
 }
