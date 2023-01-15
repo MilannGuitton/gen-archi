42bba05bf6475f38d70ceb9ee6d5ed8b6fd06099
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-01-15 18:38:46 +0100

    Remove some outputs

--- third_platform/backend/outputs.tf
+++ third_platform/backend/outputs.tf
@@ -1,25 +1,10 @@
-# Default
-output "db_instance_address" {
-  description = "The address of the RDS instance"
-  value       = module.db_spacelift_mysql.db_instance_address
-}
+# ----------------------------------------------------------------- Output --- #
 
 output "db_instance_status" {
   description = "The RDS instance status"
   value       = module.db_spacelift_mysql.db_instance_status
 }
 
-output "db_instance_name" {
-  description = "The database name"
-  value       = module.db_spacelift_mysql.db_instance_name
-}
-
-output "base_url" {
-  description = "Base URL for API Gateway stage."
-
-  value = aws_apigatewayv2_stage.default.invoke_url
-}
-
-output "custom_domain_api" {
+output "backend_url" {
   value = "https://${aws_apigatewayv2_api_mapping.genarchi.domain_name}"
 }
