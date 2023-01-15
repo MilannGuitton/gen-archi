7044b97b84f756ef458dd700351cf908a5e73dbc
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-01-15 18:14:12 +0100

    Add more restrictive sec group for RDS

--- third_platform/backend/security_group.tf
+++ third_platform/backend/security_group.tf
@@ -1,11 +1,11 @@
-# ------------------------------------------------------------------ Mysql --- #
+# ----------------------------------------------------------------- Lambda --- #
 
 module "sg_lambda_mysql" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
-  name        = "Lambda mysql sg"
-  description = "MySQL security group"
+  name        = "lambda-mysql"
+  description = "Lambda for MySQL"
   vpc_id      = module.vpc.vpc_id
 
   egress_with_cidr_blocks = [
@@ -19,39 +19,34 @@ module "sg_lambda_mysql" {
   ]
 }
 
+
+# ------------------------------------------------------------------ MySQL --- #
+
 module "sg_db_mysql" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
-  name        = var.db_name
-  description = "MySQL security group"
+  name        = "${var.db_name}-db"
+  description = "MySQL from lambda"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_with_cidr_blocks = [
+  ingress_with_source_security_group_id = [
     {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      description = "Allow all inbound traffic"
-      cidr_blocks = "0.0.0.0/0"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "TCP"
+    description = "MySQL ingress access from lambda"
+    source_security_group_id = module.sg_lambda_mysql.security_group_id
     },
-    #{
-    #from_port   = 3306
-    #to_port     = 3306
-    #protocol    = "tcp"
-    #description = "MySQL access from within VPC"
-      #cidr_blocks = module.vpc.vpc_cidr_block
-      #cidr_blocks = "0.0.0.0/0"
-      #},
   ]
 
-  egress_with_cidr_blocks = [
+  egress_with_source_security_group_id = [
     {
       from_port   = 0
       to_port     = 0
-      protocol    = "-1"
-      description = "Allow all outbound traffic"
-      cidr_blocks = "0.0.0.0/0"
+      protocol    = "TCP"
+      description = "Allow egress TCP to lambda"
+      source_security_group_id = module.sg_lambda_mysql.security_group_id
     }
   ]
 }
