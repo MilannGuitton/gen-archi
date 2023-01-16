c484517310d793172bf812f7dae59aed32a6b9aa
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-01-16 01:09:13 +0100

    Fix content type for main file

--- third_platform/frontend/website.tf
+++ third_platform/frontend/website.tf
@@ -9,3 +9,14 @@ resource "aws_s3_object" "website_files" {
   source      = "${var.static_website_path}/${each.value}"
   source_hash = filemd5("${var.static_website_path}/${each.value}")
 }
+
+resource "aws_s3_object" "index" {
+  bucket      = aws_s3_bucket.website.id
+  key         = "index.html"
+  source      = "${var.static_website_path}/index.html"
+  source_hash = filemd5("${var.static_website_path}/index.html")
+
+  content_type = "text/html"
+
+  depends_on = [aws_s3_object.website_files]
+}
